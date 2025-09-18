4724c296244c23d25d41993c2499efa75fb45bef
Vinh Nguyen <ngtv0404@gmail.com>
2025-09-18 19:37:29 +1000

    enable USB serial and add stdio Hello World code

--- src/main.c
+++ src/main.c
@@ -4,16 +4,10 @@
 #include "pico/stdlib.h"
 
 
-void led_task()
-{   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+void hello_task() {
     while (true) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        printf("Hello, World!\n");
+        vTaskDelay(1000);
     }
 }
 
@@ -21,7 +15,7 @@ int main()
 {
     stdio_init_all();
 
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(hello_task, "HelloWorld", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
